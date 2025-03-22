4308a0d513415236b2236398fa922471f49af718
Andrey <andrejfedotov012@gmail.com>
2025-03-23 00:14:17 +0200

    first commit

---
+++ prj/Fedotov-task_7_1/Fedotov-task_7_1/Fedotov-task_7_1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(65001);
+    SetConsoleCP(1251);
+
+    string answer;
+    do
+    {
+        string sentence;
+        int count = 0;
+        bool flag = false;
+
+        cout << "Введіть речення (що закінчується на '.' або '!'): ";
+        getline(cin, sentence);
+
+        char lastChar = sentence[sentence.length() - 1];
+        while (flag == false) {
+            if (lastChar != '.' && lastChar != '!') {
+                cout << "Помилка: речення має закінчуватися '.' або '!'" << endl;
+                cout << "Введіть речення знову: ";
+                getline(cin, sentence);
+                lastChar = sentence[sentence.length() - 1];
+            }
+            else {
+                flag = true;
+            }
+        }
+  
+        for (int i = 0; i < sentence.length(); i++) {
+            if (sentence[i] == 'е' || sentence[i] == 'Е' || sentence[i] == 'e' || sentence[i] == 'E') {
+                count++;
+            }
+        }
+     
+        if (count > 0) {
+            cout << "Кількість символів 'е' або 'Е' у реченні: " << count << endl;
+        }
+        else {
+            cout << "Символ 'е' або 'Е' відсутній у введеному реченні." << endl;
+        }
+       
+        cout << "Чи хочете спробувати заново? (так/ні): ";
+        cin >> answer;
+        cin.ignore(); 
+
+    } while (answer == "так" || answer == "Так" || answer == "ТАК" );
+    cout << "Програма завершена!";
+    system("pause"); 
+    return 0;
+}
+
+
---
+++ prj/Fedotov-task_7_2/Fedotov-task_7_2/Fedotov-task_7_2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(65001);
+    SetConsoleCP(1251);
+
+    int arr [20];  
+    cout << "Введіть 20 натуральних чисел:\n";
+    for (int i = 0; i < 20; i++) {
+        cin >> arr[i];
+    }
+       
+    int evenCount = 0, oddCount = 0;
+
+    for (int i = 0; i < 20; i++) {
+        if (arr[i] % 2 == 0) {
+            evenCount++;
+        }
+        else {
+            oddCount++;
+        }
+    }
+
+    if (evenCount > oddCount) {
+        cout << "Більше парних чисел.\n";
+    }
+    else if (oddCount > evenCount) {
+        cout << "Більше непарних чисел.\n";
+    }
+    else {
+        cout << "Кількість парних і непарних чисел однакова.\n";
+    }
+    system("pause");
+    return 0;
+}
